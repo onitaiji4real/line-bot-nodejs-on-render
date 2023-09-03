83d3f5701b7ef43b8b3bd780cdf186a6f1b7c4a9
onitaiji4real <minecraft8810@gmail.com>
2023-09-04 06:02:04 +0800

    test5

--- app.js
+++ app.js
@@ -29,10 +29,6 @@ bot.on("message", function (event) {
 });
 
 //bot 所監聽的webhook路徑與port
-bot.listen(
-  "https://linebottest2.onrender.com/callback",
-
-  function () {
-    console.log("[廢物小幫手已準備就緒！2]");
-  }
-);
+bot.listen("https://linebottest2.onrender.com/callback", 3000, function () {
+  console.log("[廢物小幫手已準備就緒！2]");
+});
